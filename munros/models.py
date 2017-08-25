649a8482fd7d83c7be12d4e1d4daadf4e3fea87c
John Allan <johnjamesharvey@gmail.com>
2017-08-25 13:53:01 +0100

    psql database added

---
+++ munros/models.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Mountain(models.Model):
+  name= models.CharField(max_length= 200)
+  height= models.CharField(max_length=20)
+  lat= models.DecimalField(max_digits=9, decimal_places=6)
+  lon= models.DecimalField(max_digits=9, decimal_places=6)
+  created_at= models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return '%s %s' % (self.name, self.height) 
